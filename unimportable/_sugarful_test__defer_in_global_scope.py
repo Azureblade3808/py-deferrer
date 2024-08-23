b04a73ec632c9c72d1ffb6198ecc829907cbd6d2
傅立业（Chris Fu） <17433201@qq.com>
2024-08-23 18:07:36 +0800

    Move unimportable modules out of `deferrer`
    ..., so that doctest can run without errors.

--- unimportable/_sugarful_test__defer_in_global_scope.py
+++ unimportable/_sugarful_test__defer_in_global_scope.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 __all__ = []
 
-from ._sugarful import defer
+from deferrer._sugarful import defer
 
 # The following line should cause a `RuntimeError`.
 defer and print()
--- unimportable/_sugarless_test__defer_in_global_scope.py
+++ unimportable/_sugarless_test__defer_in_global_scope.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 __all__ = []
 
-from ._sugarless import defer
+from deferrer._sugarless import defer
 
 # The following line should cause a `RuntimeError`.
 defer(print)()
