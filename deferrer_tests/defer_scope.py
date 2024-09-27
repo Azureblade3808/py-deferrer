29e00214789835de80c2123595b0b4aca6a2b0c0
傅立业（Chris Fu） <17433201@qq.com>
2024-09-27 18:55:06 +0800

    Rework tests to follow naming conventions (classes and functions)

--- deferrer_tests/defer_scope.py
+++ deferrer_tests/defer_scope.py
@@ -1,110 +1,107 @@
 from __future__ import annotations
 
+from deferrer import defer, defer_scope
 
-def defer_scope_can_be_used_to_provide_context_manager() -> None:
-    """
-    If `defer_scope` is called with no argument, it provides a context
-    manager that holds all deferred actions. When the context manager
-    exists, the deferred actions it holds get performed.
-    """
 
-    from deferrer import defer, defer_scope
+class Test__defer_scope:
+    @staticmethod
+    def test__can_be_used_to_provide_context_manager() -> None:
+        """
+        If `defer_scope` is called with no argument, it provides a
+        context manager that holds all deferred actions. When the
+        context manager exists, the deferred actions it holds get
+        performed.
+        """
 
-    nums = []
+        nums = []
 
-    with defer_scope() as _scope_0:
-        defer and nums.append(0)
-        assert nums == []
-
-        # `defer_scope` can be nested.
-        with defer_scope() as _scope_1:
-            defer and nums.append(1)
+        with defer_scope() as _scope_0:
+            defer and nums.append(0)
             assert nums == []
 
-            nums.append(2)
-            assert nums == [2]
+            # `defer_scope` can be nested.
+            with defer_scope() as _scope_1:
+                defer and nums.append(1)
+                assert nums == []
 
-            defer and nums.append(3)
-            assert nums == [2]
+                nums.append(2)
+                assert nums == [2]
 
-        # Exits `_scope_1`.
-        assert nums == [2, 3, 1]
+                defer and nums.append(3)
+                assert nums == [2]
 
-        defer and nums.append(4)
-        assert nums == [2, 3, 1]
+            # Exits `_scope_1`.
+            assert nums == [2, 3, 1]
 
-    # Exits `_scope_0`.
-    assert nums == [2, 3, 1, 4, 0]
+            defer and nums.append(4)
+            assert nums == [2, 3, 1]
 
+        # Exits `_scope_0`.
+        assert nums == [2, 3, 1, 4, 0]
 
-def defer_is_allowed_to_be_used_in_module_level_with_defer_scope() -> None:
-    """
-    Inside a `defer_scope()` context, `defer` is allowed to be used in
-    module level.
-    """
+    @staticmethod
+    def test__enclosing_defer_is_allowed_to_be_used_at_module_level() -> None:
+        """
+        Inside a `defer_scope()` context, `defer` is allowed to be used
+        at module level.
+        """
 
-    from deferrer_tests.samples import sugarless_with_defer_scope as _
+        from deferrer_tests.samples import sugarless_with_defer_scope as _
 
+    @staticmethod
+    def test__enclosing_defer_is_allowed_to_be_used_at_class_level() -> None:
+        """
+        Inside a `defer_scope()` context, `defer` is allowed to be used
+        at class level.
+        """
 
-def defer_is_allowed_to_be_used_in_class_level_with_defer_scope() -> None:
-    """
-    Inside a `defer_scope()` context, `defer` is allowed to be used in
-    class level.
-    """
+        class _:
+            with defer_scope():
 
-    from deferrer import defer, defer_scope
+                @defer
+                def _() -> None:
+                    pass
 
-    class _:
-        with defer_scope():
+    @staticmethod
+    def test__can_be_used_to_wrap_functions() -> None:
+        """
+        When called with a function as the argument, `defer_scope(...)`
+        ensures that `defer` should be usable in the function (even in
+        Python prior to 3.12).
+        """
 
-            @defer
-            def _() -> None:
-                pass
+        nums = []
 
+        @defer_scope
+        def f() -> None:
+            nums.clear()
+            assert nums == []
 
-def defer_scope_can_be_used_to_wrap_functions() -> None:
-    """
-    When called with a function as the argument, `defer_scope(...)`
-    ensures that `defer` should be usable in the function (even in
-    Python prior to 3.12).
-    """
+            defer and nums.append(0)
+            assert nums == []
 
-    from deferrer import defer, defer_scope
+            nums.append(1)
+            assert nums == [1]
 
-    nums = []
+            defer and nums.append(2)
+            assert nums == [1]
 
-    @defer_scope
-    def f() -> None:
-        nums.clear()
-        assert nums == []
+        f()
+        assert nums == [1, 2, 0]
 
-        defer and nums.append(0)
-        assert nums == []
+    @staticmethod
+    def test__can_be_used_to_wrap_iterables() -> None:
+        """
+        When called with an iterable as the argument, `defer_scope(...)`
+        returns a similar iterable with one distinct `defer_scope`
+        context for each iteration.
+        """
 
-        nums.append(1)
-        assert nums == [1]
+        nums = []
 
-        defer and nums.append(2)
-        assert nums == [1]
+        for i in defer_scope([1, 2, 3]):
+            defer and nums.append(-1)
+            nums.append(i)
+            defer and nums.append(0)
 
-    f()
-    assert nums == [1, 2, 0]
-
-
-def defer_scope_can_be_used_to_wrap_iterables() -> None:
-    """
-    When called with an iterable as the argument, `defer_scope(...)`
-    returns a similar iterable with one distinct `defer_scope` context
-    for each iteration.
-    """
-
-    from deferrer import defer, defer_scope
-
-    nums = []
-
-    for i in defer_scope([1, 2, 3]):
-        defer and nums.append(-1)
-        nums.append(i)
-        defer and nums.append(0)
-
-    assert nums == [1, 0, -1, 2, 0, -1, 3, 0, -1]
+        assert nums == [1, 0, -1, 2, 0, -1, 3, 0, -1]
